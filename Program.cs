7942d91a7c0c1ba4183aa4eaadbd7720a409b4ec
Devanshu Pathak <90167731+Devansh968@users.noreply.github.com>
2023-08-25 11:47:32 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using FirstWebAPI_March16.Repo;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSingleton<IBook, SQLConnection>();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
